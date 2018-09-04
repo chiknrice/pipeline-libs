ed6c57e9e61db7135bebf29ec51d7d043388f328
ian <chiknrice@gmail.com>
2018-09-05 01:13:31 +1200

    working search

--- vars/buildRuns.groovy
+++ vars/buildRuns.groovy
@@ -1,13 +1,14 @@
-import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 
 def withBuildVars(Map filter) {
     def matches = []
     def previousBuild = currentBuild.previousBuild
     while(previousBuild) {
         def buildVariables = previousBuild.buildVariables
+        println "Inspecting ${previousBuild.number} which contains ${buildVariables}"
         def tempFilter = filter.clone()
         def result = tempFilter - buildVariables
         if(result.empty) {
+            println "Matched ${previousBuild.number}"
             matches << previousBuild
         }
         previousBuild = previousBuild.previousBuild
